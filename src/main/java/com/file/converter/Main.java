49db7f213c3b51b7e1bfbe324249b7662c1b97f9
AntonYakovenko <antonyakovenko96@gmail.com>
2019-03-22 22:28:08 +0200

    Simple convertion json to yaml

--- src/main/java/com/file/converter/Main.java
+++ src/main/java/com/file/converter/Main.java
@@ -1,7 +1,20 @@
 package com.file.converter;
 
-public class Main {
-    public static void main(String[] args) {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String jsonString = "{\"k1\":\"v1\",\"k2\":\"v2\"}";
+
+        // parse JSON
+        JsonNode jsonNodeTree = new ObjectMapper().readTree(jsonString);
+        // save it as YAML
+        String jsonAsYaml = new YAMLMapper().writeValueAsString(jsonNodeTree);
+
+        System.out.println(jsonAsYaml);
     }
 }
